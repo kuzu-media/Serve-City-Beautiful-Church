1d618e183cd1d2310c998c075b6f60cddc4b2523
Rachel Higley <me@rachelhigley.com>
2013-05-04 19:02:40 -0400

    Update the rules of validation

--- system/models/Member.php
+++ system/models/Member.php
@@ -6,22 +6,32 @@ Class Member extends Model
 
 	public $hasMany = array('ShiftMember','TeamMember');
 
-	public $required = array('name','email','profile_pic');
+	public $required = array('name','email','profile_pic','alert_type_id');
 
 	public $rules = array(
-		'id' => array('numeric','maxLength' =>11), 
-		'facebook_id' => array('numeric','maxLength' =>11), 
-		'name' => array('alphaNumeric','maxLength' =>250), 
-		'email' => array('alphaNumeric','maxLength' =>250), 
-		'phone' => array('alphaNumeric','maxLength' =>13), 
-		'profile_pic' => array(), 
-		'times' => array('numeric','maxLength' =>1), 
-		'reminder_day_id' => array('numeric','maxLength' =>1), 
-		'member_type_id' => array('numeric','maxLength' =>11), 
-		'alert_type_id' => array('numeric','maxLength' =>10), 
-		'login_type_id' => array('numeric','maxLength' =>11), 
+		'id' => array('numeric','maxLength' =>11),
+		'facebook_id' => array('numeric','maxLength' =>40),
+		'name' => array('alphaNumeric','maxLength' =>250),
+		'email' => array('email','maxLength' =>250),
+		'phone' => array('alphaNumeric','maxLength' =>13),
+		'profile_pic' => array(),
+		'times' => array('numeric','maxLength' =>1),
+		'reminder_day_id' => array('numeric','maxLength' =>1),
+		'member_type_id' => array('numeric','maxLength' =>11),
+		'alert_type_id' => array('numeric','maxLength' =>10),
+		'login_type_id' => array('numeric','maxLength' =>11),
 		'password' => array('alphaNumeric','maxLength' =>205)
 		);
 
+	public function check_id($id)
+	{
+		$this->options['recursive'] = 0;
 
+		$user = $this->findByFacebookId($id);
+
+		if($this->success) return $user;
+
+		return $this->success;
+
+	}
 }
