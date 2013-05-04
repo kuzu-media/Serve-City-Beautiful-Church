c77dccb5a88aca8200f7940878370c8d0cb6f3aa
Rachel Higley <me@rachelhigley.com>
2013-05-04 10:00:51 -0400

    Home page html

--- system/views/team/index.php
+++ system/views/team/index.php
@@ -1,32 +1,21 @@
-<div class='table'>
-	<div class='row'>
-		<div class='col'>id</div>
-		<div class='col'>name</div>
-		<div class='col'>photo</div>
-		<div class='col'>summary</div>
-		<div class='col'>video</div>
-		<div class='col'>content</div>
+<div class="row">
+	<div class="bucket cols">
+		<h1>Welcome to Operations</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, sequi, voluptates itaque reiciendis minus eaque porro dignissimos unde incidunt veniam quidem eos nulla quisquam inventore nesciunt cupiditate nemo? Dolorum, ex.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque ad cumque enim eligendi dignissimos soluta dolorem. Fugit, eius accusamus repellat repudiandae illo numquam provident unde corporis necessitatibus vero totam aut.</p>
 	</div>
-	<?php foreach($teams as $team):?>
-		<div class='row'>
-			<div class='col'>
-				<?php echo $team['id'] ?>
-			</div>
-			<div class='col'>
-				<?php echo $team['name'] ?>
-			</div>
-			<div class='col'>
-				<?php echo $team['photo'] ?>
-			</div>
-			<div class='col'>
-				<?php echo $team['summary'] ?>
-			</div>
-			<div class='col'>
-				<?php echo $team['video'] ?>
-			</div>
-			<div class='col'>
-				<?php echo $team['content'] ?>
-			</div>
+</div>
+<div class="row">
+<h2 id="areas">Areas to Serve</h2>
+</div>
+
+<div class="row">
+<?php foreach($teams as $team):?>
+	<div class="cols col-4">
+		<div class="bucket">
+			<?php echo Asset::img($team['photo'])?>
+			<h3><a href="<?php echo  Asset::create_url("team","get",array($team['id']))?>"><?php echo $team['name']?></a></h3>
+			<p><?php echo $team['summary']?></p>
 		</div>
-	<?php endforeach ?>
+	</div>
+<?php endforeach; ?>
 </div>
