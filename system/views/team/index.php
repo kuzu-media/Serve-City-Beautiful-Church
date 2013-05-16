5d83b9de40e15ff7e891303c43c8e11c17d59e7c
Rachel Higley <me@rachelhigley.com>
2013-05-16 18:00:29 -0400

    Remove operations from the wording.

--- system/views/team/index.php
+++ system/views/team/index.php
@@ -1,6 +1,6 @@
 <div class="row">
 	<div class="bucket cols">
-		<h1>Welcome to Operations</h1>
+		<h1>Welcome!</h1>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, sequi, voluptates itaque reiciendis minus eaque porro dignissimos unde incidunt veniam quidem eos nulla quisquam inventore nesciunt cupiditate nemo? Dolorum, ex.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque ad cumque enim eligendi dignissimos soluta dolorem. Fugit, eius accusamus repellat repudiandae illo numquam provident unde corporis necessitatibus vero totam aut.</p>
 	</div>
 </div>
