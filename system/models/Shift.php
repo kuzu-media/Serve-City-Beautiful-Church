ea04e32d15dc55c16b6a680778b231559d041fc8
Rachel Higley <me@rachelhigley.com>
2013-05-21 10:42:43 -0400

    update for max Length on time

--- system/models/Shift.php
+++ system/models/Shift.php
@@ -9,10 +9,10 @@ Class Shift extends Model
 	public $required = array('date_id','team_id','time');
 
 	public $rules = array(
-		'id' => array('numeric','maxLength' =>11), 
-		'date_id' => array('numeric','maxLength' =>11), 
-		'team_id' => array('numeric','maxLength' =>11), 
-		'time' => array('alphaNumeric','maxLength' =>8), 
+		'id' => array('numeric','maxLength' =>11),
+		'date_id' => array('numeric','maxLength' =>11),
+		'team_id' => array('numeric','maxLength' =>11),
+		'time' => array('alphaNumeric','maxLength' =>250),
 		'notes' => array()
 		);
 
